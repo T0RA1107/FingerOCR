f6f17879d407f85601699a99cff34564202a2cce
T0RA1107 <torapira@icloud.com>
2023-10-29 11:43:05 +0900

    add basic function of finger OCR

--- Effect/EffectController.py
+++ Effect/EffectController.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import threading
 from playsound import playsound
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 import sys
 sys.path.append("./Effect")
 from pumpkin import pumpkin
@@ -16,6 +17,8 @@ class EffectController:
     def __init__(self):
         self.effect_name = None
         self.effect_cap = None
+        self.processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
+        self.model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-handwritten")
 
     def effect(self, frame):
         if self.effect_name is None: return frame
@@ -43,3 +46,24 @@ class EffectController:
             self.effect_cap = self.name2effect[effect_name]["capture"]()
             if "SE" in self.name2effect[effect_name]:
                 threading.Thread(target=playsound, args=(self.name2effect[effect_name]["SE"](),)).start()
+
+    def _recognize(self, image):
+        pixel_values = self.processor(image, return_tensors="pt").pixel_values
+        generated_ids = self.model.generate(pixel_values)
+
+        generated_text = self.processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        return generated_text
+
+    def _pre_process(self, image):
+        # 指文字をモデルが認識できる状態に修正する
+        return image
+
+    def _post_process(self, text):
+        # ハミング距離などをもとに登録されたエフェクト名と近いものに修正できるなら修正する
+        return text
+
+    def recognize_finger_writing(self, image):
+        image = self._pre_process(image)
+        text = self._recognize(image)
+        text = self._post_process(text)
+        return text
