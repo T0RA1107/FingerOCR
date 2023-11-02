a8a6493f9b7609189501a1efd828586db95feb00
T0RA1107 <torapira@icloud.com>
2023-11-02 10:20:45 +0900

    add effect abstract class

--- Effect/EffectController.py
+++ Effect/EffectController.py
@@ -5,47 +5,32 @@ from playsound import playsound
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 import sys
 sys.path.append("./Effect")
-from pumpkin import pumpkin
-from thunder import thunder, thunder_capture, thunder_SE
+import BindEffect
 
 class EffectController:
-    name2effect = {
-        "pumpkin": { "call": pumpkin, "type": "sticker" },
-        "thunder": { "call": thunder, "type": "movie", "capture": thunder_capture, "SE": thunder_SE }
-    }
+    name2effect = BindEffect.name2effect
 
     def __init__(self):
-        self.effect_name = None
-        self.effect_cap = None
+        self.Effect = None
         self.processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
         self.model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-handwritten")
 
     def effect(self, frame):
-        if self.effect_name is None: return frame
-        Effect = self.name2effect[self.effect_name]["call"]
-        if self.name2effect[self.effect_name]["type"] == "sticker":
-            return Effect(frame)
-        elif self.name2effect[self.effect_name]["type"] == "movie":
-            ret, effect_image = self.effect_cap.read()
-            if not ret:
-                self.reset()
-                return frame
-            else:
-                Effect = self.name2effect[self.effect_name]["call"]
-                return Effect(frame, effect_image)
+        if self.Effect is None: return frame
+        ret, effected_image = self.Effect(frame)
+        if not ret:
+            self.reset()
+        return effected_image
 
     def reset(self):
-        if self.name2effect[self.effect_name]["type"] == "movie":
-            self.effect_cap = None
-        self.effect_name = None
+        self.Effect = None
 
     def set_effect(self, effect_name):
         assert effect_name in self.name2effect, f"effect \"{effect_name}\" does not exist"
-        self.effect_name = effect_name
-        if self.name2effect[effect_name]["type"] == "movie":
-            self.effect_cap = self.name2effect[effect_name]["capture"]()
-            if "SE" in self.name2effect[effect_name]:
-                threading.Thread(target=playsound, args=(self.name2effect[effect_name]["SE"](),)).start()
+        self.Effect = self.name2effect[effect_name]()  # インスタンス化
+        if self.Effect.type == "movie":
+            if self.Effect.SE is not None:
+                threading.Thread(target=playsound, args=(self.Effect.SE,)).start()
 
     def _recognize(self, image):
         pixel_values = self.processor(image, return_tensors="pt").pixel_values
